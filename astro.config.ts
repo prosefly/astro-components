932e52d1ea0311d858fbecbf15f928d4003fcdca
Hsiaoming Yang <me@lepture.com>
2026-08-23 14:26:21 +0900

    fix: cleanup local dev site

--- astro.config.ts
+++ astro.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'astro/config';
 import lotus from '@prosefly/astro-theme-lotus';
-import {
-  remarkCalloutDirectives,
-  unified,
-} from '@prosefly/astro-components/markdown';
 
 export default defineConfig({
   integrations: [lotus()],
-  markdown: {
-    processor: unified({
-      remarkPlugins: [remarkCalloutDirectives],
-    }),
-  },
 });
